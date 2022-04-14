b57faae13e5f233b64023e4b3d809bf28a8897e2
Martin Saidl <martin.saidl@tone.cz>
2022-04-14 11:17:48 +0100

    Initial commit

---
+++ scripts/entrypoint-twampd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+DATA_DIR=${DATA_DIR}
+
+create_twamp_dir() {
+  # populate default twamp-server configuration if it does not exist
+  if [ ! -f ${DATA_DIR}/twamp-server.conf ]; then
+    cp /etc/twamp-server/twamp-server.conf ${DATA_DIR}/
+  fi
+
+  if [ ! -f ${DATA_DIR}/twamp-server.limits ]; then
+    cp /etc/twamp-server/twamp-server.limits ${DATA_DIR}/
+  fi
+
+}
+
+create_twamp_dir
+
+echo "Starting supervisord..."
+exec $(which supervisord) -c /etc/supervisor.conf
---
+++ scripts/firewall.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+# TWAMP-Responder firewall script v 1.0 
+# (c) 2022 Martin Saidl@tone.cz
+
+# Environment vars
+# CHAIN - iptables chain
+# IP - TWAMPD IP address
+# DATA_DIR - configuration directory
+
+
+ALLOW="hosts.allow"
+
+# Cleanup 
+cleanup() {
+  echo "Cleaning up ..."
+  iptables -D FORWARD -j $CHAIN
+  iptables -F $CHAIN
+  iptables -X $CHAIN
+  echo "done."
+  exit 0
+}
+
+# Signal handling
+trap cleanup INT TERM STOP KILL
+
+# Main
+echo "Configuring firewall for TWAMP responder ..."
+echo "TAMP Chain: $CHAIN"
+echo "TWAMP IP: $IP"
+
+echo "Existing $CHAIN rules:"
+iptables -S $CHAIN
+if [ 0 -ne $? ]; then
+  iptables -N $CHAIN
+fi
+
+echo "Flushing $CHAIN chain."
+iptables -D FORWARD -j $CHAIN
+iptables -F $CHAIN
+
+# Output rules
+iptables -A $CHAIN -s $IP -m state --state RELATED,ESTABLISHED -j ACCEPT 
+
+# Input rules
+if [ -f "$DATA_DIR/$ALLOW" ]; then
+  echo "Processing $ALLOW ..."
+  while read SRCIP; do
+    if [[ $SRCIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      echo "  Adding $SRCIP to $CHAIN chain"
+      iptables -A $CHAIN -d $IP -s $SRCIP -j ACCEPT
+    fi
+  done < $DATA_DIR/$ALLOW
+  echo "done."
+fi
+
+# Final drop
+echo "Adding final drop into $CHAIN chain"
+iptables -A $CHAIN -s $IP -j DROP
+iptables -A $CHAIN -d $IP -j DROP
+
+echo "Current $CHAIN chain:"
+iptables -S $CHAIN
+
+iptables -I FORWARD -j $CHAIN
+
+echo "done."
+
+while true; do
+  sleep 1 &
+  wait $!
+done
+
+exit 0
